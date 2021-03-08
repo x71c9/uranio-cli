ffc6f4fc7406f2b8c9b9994a31f21a558b544be8
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-03-08 12:35:40 +0100

    reset package-lock.

--- dist/cmd/init.d.ts
+++ dist/cmd/init.d.ts
@@ -3,7 +3,6 @@
  *
  * @packageDocumentation
  */
-import { Arguments } from '../types';
 export declare const init: {
-    run: (args: Arguments) => Promise<void>;
+    run: (args: any) => any;
 };
