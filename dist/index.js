f5286bf65a2b2a2bb9b51ef33a52f8b25ef1700a
Andrea Reni <mail@nbl7.net>
2021-07-11 13:24:49 +0200

    generating clients books.

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cmd_1 = require("./cmd/");
 exports.default = {
     init: cmd_1.init.run,
-    transpose: cmd_1.transpose.run
+    transpose: cmd_1.transpose.run,
+    alias: cmd_1.alias.run
 };
 //# sourceMappingURL=index.js.map
